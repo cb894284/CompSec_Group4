5ff72c674177e034d5b3b38e24109d8442c6dcd2
cb894284 <45403501+cb894284@users.noreply.github.com>
2018-11-29 14:11:54 -0500

    Specify directory for Anaconda install

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ sudo systemct1 enable apache2
 
 #setup anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
-sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
+sudo bash -c "bash ~/Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
 sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
 sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
 
